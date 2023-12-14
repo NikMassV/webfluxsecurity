f1afddca6bbc8ae78ee956849024d68700ed21ce
NikMassV <slipknoshka777@gmail.com>
2023-12-14 15:40:53 +0100

    init

---
+++ src/main/java/edu/nm/webfluxsecurity/WebfluxsecurityApplication.java
@@ -0,0 +1,13 @@
+package edu.nm.webfluxsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nm/webfluxsecurity/WebfluxsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nm.webfluxsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxsecurityApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
